82ffe8e417fb04e80b3b79da97e17bb2c6b77c3b
Tomas-Paoletti <tomiagustin.tp@gmail.com>
2023-03-05 20:57:54 -0300

    subiendo seccion de inicio de turno

--- django/Iot/login/urls.py
+++ django/Iot/login/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_user, name="login"),
+    path('shift/', views.start_shift, name="shift")
 ]
--- django/Iot/login/views.py
+++ django/Iot/login/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse
+from django.db import connection
+from django.utils import timezone
+
+from django.utils import timezone
+
+timezone.activate('America/Argentina/Buenos_Aires')
 
 
 def login_user(request):
@@ -21,3 +27,26 @@ def login_user(request):
             return redirect('login')
     else:  # sino esta logueado redirija a la pagina
         return render(request, 'login/login.html', {})
+
+
+def start_shift(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        shift = request.POST.get("turno")
+        line = request.POST.getlist("lines[]")
+        if shift is not None and line:
+            # print(shift, line)
+            with connection.cursor() as cursor:
+                for lines in line:
+                    cursor.execute(
+                        "INSERT INTO linemanagement_line_status (lineName, shift,userId_id,starTime) VALUES (%s, %s,%s,%s)", (lines, shift, 3, timezone.now()))
+
+        else:
+
+            # Handle missing keys
+            pass
+        redirect("shift")
+
+    name = "Ian"
+    num_lines = 3
+    return render(request, "login/turno.html", {"name": name, "num_lines": num_lines})
